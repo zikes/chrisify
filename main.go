f119e73d708c29da96e48f9e99db7e23ff07d351
Jason Hutchinson <zikes@zikes.me>
2017-02-16 22:50:45 -0600

    update relative file path for sub-package

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"chrisify/facefinder"
 	"flag"
 	"image"
 	"image/draw"
@@ -9,6 +8,8 @@ import (
 	_ "image/png"
 	"os"
 	"path/filepath"
+
+	"github.com/zikes/chrisify/facefinder"
 
 	"github.com/disintegration/imaging"
 )
